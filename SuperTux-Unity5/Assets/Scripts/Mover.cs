f7c3efc10c6d0f475799d1a63bd58828ebc34266
Diogo RBG <diogorbg@gmail.com>
2016-09-19 11:09:15 -0300

    mover

---
+++ SuperTux-Unity5/Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public Vector2 vel;
+
+	void Update () {
+		float dt = Time.deltaTime;
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.position += vel.x * Vector3.right * dt;
+		else if (Input.GetKey(KeyCode.LeftArrow))
+			transform.position += vel.x * Vector3.left * dt;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+			transform.position += vel.y * Vector3.up * dt;
+		else if (Input.GetKey(KeyCode.DownArrow))
+			transform.position += vel.y * Vector3.down * dt;
+	}
+
+	//public
+
+}
