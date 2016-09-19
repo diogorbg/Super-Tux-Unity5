e9a64a21afd4c6d0435e17d1b5e27340e67b4132
Diogo RBG <diogorbg@gmail.com>
2016-09-19 11:58:35 -0300

    mover por mouse ou teclado

--- SuperTux-Unity5/Assets/Scripts/Mover.cs
+++ SuperTux-Unity5/Assets/Scripts/Mover.cs
@@ -6,6 +6,11 @@ public class Mover : MonoBehaviour {
 	public Vector2 vel;
 
 	void Update () {
+		moverTeclado ();
+		moverMouse ();
+	}
+
+	public void moverTeclado () {
 		float dt = Time.deltaTime;
 		if (Input.GetKey(KeyCode.RightArrow))
 			transform.position += vel.x * Vector3.right * dt;
@@ -18,6 +23,18 @@ public class Mover : MonoBehaviour {
 			transform.position += vel.y * Vector3.down * dt;
 	}
 
-	//public
+	public void moverMouse () {
+		float dt = Time.deltaTime;
+	
+		if (!Input.GetKey (KeyCode.Mouse0))
+			return;
+	
+		Vector3 p = Input.mousePosition;
+		p.x -= Screen.width / 2;
+		p.y -= Screen.height / 2;
+		transform.position +=
+			Vector3.right * Mathf.Cos(Mathf.Atan2(p.y, p.x)) * vel.x * dt +
+			Vector3.up * Mathf.Sin(Mathf.Atan2(p.y, p.x)) * vel.y * dt;
+	}
 
 }
